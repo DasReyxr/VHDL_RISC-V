b2a7860574cea7fcb8edaed0465cf58a487ccbf3
DasReyxr <contreras.orlandoxv@gmail.com>
2025-01-24 11:48:47 -0600

    First Release Virtual_RISC.vhd

--- Test/RISCV_Speed.vhd
+++ Test/RISCV_Speed.vhd
@@ -1,7 +1,8 @@
 ----------- Code -----------
 --------- Auf Das ---------
 ------ RISCV ------
--------- 03/01/2025 --------
+--- I. Date: 03/01/2025 ---
+--- C. Date: 16/01/2025 ---
 ------- Main Library -------
 library IEEE;
 use IEEE.STD_LOGIC_1164.all; 
@@ -13,6 +14,7 @@ entity RISCV is
 	(
         ---- Main Clock ---
         RESETV, CLK : in std_logic;
+        B1UP,B2DOWN : in std_logic;
         -- Rom & Ram --
         --ROMIN: in std_logic_vector (7 downto 0);
         RAMIN : inout std_logic_vector (7 downto 0);
@@ -37,15 +39,30 @@ architecture juve3dstudio of RISCV is
     constant  M_P0IN         :std_logic_vector(2 downto 0)  := "110";
     constant  M_INA          :std_logic_vector(2 downto 0)  := "111";
     -- Virtual Rom -- 
-    signal  ROMIN: std_logic_vector (7 downto 0) :="00000000";
-    type MEMORY is array (0 to 15) of std_logic_vector(7 downto 0);
+    signal  ROMIN, S_P1OUT: std_logic_vector (7 downto 0) :="00000000";
+--    signal S_P1OUT: std_logic_vector (7 downto 0) :="00000000";
+    type MEMORY is array (0 to 33) of std_logic_vector(7 downto 0);
+    constant VROMIN : MEMORY := (x"57",
+        x"01", x"50", x"45", x"50", -- Data values for addresses 0 to 3
+        x"45", x"50", x"45", x"50", -- Data values for addresses 4 to 7
+        x"45", x"50", x"45", x"50", -- Data values for addresses 8 to 11
+        x"45", x"50", x"45", x"50",  -- Data values for addresses 12 to 15
+        x"46", x"50", x"46", x"50",
+        x"46", x"50", x"46", x"50",
+        x"46", x"50", x"46", x"50",
+        x"46", x"50",x"46", x"50",x"44" ,
+        others => x"FF"             -- Remaining addresses 16 to 255 filled with zeros
+    );
+
+    /* Barrido LEds
     constant VROMIN : MEMORY := (
         x"42", x"50", x"45", x"50", -- Data values for addresses 0 to 3
         x"45", x"50", x"45", x"50", -- Data values for addresses 4 to 7
-        x"45", x"50", x"45", x"50", -- Data values for addresses 8 to 11
-        x"45", x"50", x"45", x"50"  -- Data values for addresses 12 to 15
+        x"46", x"50", x"46", x"50", -- Data values for addresses 8 to 11
+        x"46", x"50", x"46", x"50"  -- Data values for addresses 12 to 15
     );
-    signal VRAMIN : MEMORY := (others => x"FF");
+    */
+   signal VRAMIN : MEMORY := (others => x"FF");
     signal S_ADDRESSRAM : unsigned(7 downto 0);
 
 
@@ -54,17 +71,18 @@ architecture juve3dstudio of RISCV is
     signal RESET : std_logic:= '1';
 
     -- Program Counter --
-    signal S_PCIN,PC_A : unsigned (7 downto 0) := "00000000";
+    signal S_PCIN,PC_A,PC_S : unsigned (7 downto 0) := "00000000";
     
     
     -- BUSES DE SLECTOR DE DATOS Y CSC --
-    signal P1, SELR : std_logic_vector(7 downto 0);
+    signal P1, SELR : std_logic_vector(7 downto 0) := (others => '0');
     signal XIN, DATA : unsigned (7 downto 0) := (others => '0'); 
-    signal SPIN: unsigned (7 downto 0) := x"FF";
+    signal SPIN: unsigned (7 downto 0) := x"21";
 
     -- MUX RAM --
     signal SELL : std_logic_vector (1 downto 0) := (others => '0');
     signal RW : std_logic;
+    SIGNAL VALOR : natural:=18;
     --MUX DATA--
     signal SELD : std_logic_vector (2 downto 0) := (others => '0');
     -- Flags --
@@ -74,37 +92,41 @@ architecture juve3dstudio of RISCV is
     signal AC, REG : signed(8 downto 0) := (others => '0');
     signal INA,CPC : signed( 8 downto 0)  := (others => '0');
     
-    signal CARRY, NEG, OVF, ZERO: std_logic;
+    signal F_CARRY, F_NEG, F_OVF, F_ZERO: std_logic;
     
 
     -- Instruction Decoder  --
     signal S_WRAM, EN_PORT, EN_AC, PCL: std_logic;
     signal IBI  : std_logic_vector(1 downto 0) := (others => '0');
-    signal SCL,IBO  : std_logic_vector (1 downto 0) := (others => '0');
-    signal SCR, SCD : std_logic_vector (2 downto 0) := (others => '0');
+    signal IBO  : std_logic_vector (1 downto 0) := (others => '0');
     
     signal SPUD, XUD : std_logic_vector (1 downto 0) ;
-    
+      
     -- 1 Second Counter --
-    signal c, cplus : unsigned(26 downto 0) := (others => '0'); -- 25 Bits 
-    constant tim : integer := 33554431;
-
+    signal c, cplus : unsigned(25 downto 0) := (others => '0'); -- 25 Bits 
+    --signal VELOCIDAD : unsigned(25 downto 0) := (others => '0'); -- 25 Bits 
 begin
+--VELOCIDAD<='0'& C(23 downto 0) when B1UP ='1' else  C(24 downto 1) & '0' when B2DOWN='1' else VELOCIDAD;
+    VALOR<= VALOR + 1 when B1UP = '1' and VALOR < 25 else 
+            VALOR - 1 when B2DOWN = '1' and VALOR > 0 else VALOR;
     --Debugging --
     RESET<=RESETV;
     ----- 1 Second Counter ----- 
     -- Memoria --
     c <= cplus when clk'event and clk='1';
     -- Logica de estado Siguiente --
-    cplus <= c + 1 when (c < "111111111111111111111111111") else (others => '0');
+    cplus <= c + 1 when (c < "11001101111111100110000000") else (others => '0');
     
-    CLKS <= CLK; --cplus(26);
+    CLKS <= C(VALOR); --cplus(26);
 
-    P_DEBUG <= not ("00000" &EN_AC & EN_PORT & PCL); 
-            --not std_logic_vector(ALU);
-            
-            --not std_logic_vector(P1OUT);
-    
+    P_DEBUG <= 
+        --not std_logic_vector(AC(7 downto 0));
+        --not std_logic_vector(DATA);
+        --not std_logic_vector(ALU);
+        --not ("00000" &EN_AC & EN_PORT & PCL);
+        not std_logic_vector(S_P1OUT);
+        --not ROMIN;
+        --not std_logic_vector(PC_A);
     
     -- Internal Memory --
     ROMIN <= VROMIN(to_integer(S_PCIN));
@@ -120,24 +142,29 @@ begin
     ADDRESSRAM <= std_logic_vector(S_ADDRESSRAM);
     -- RAM --
     --WRAM<=S_WRAM; 
-    --RAMIN <= "ZZZZZZZZ" when S_WRAM = '0' else std_logic_vector(DATA);
-    --RW <= S_WRAM and (not CLKS);
-    RAMIN <= VRAMIN(to_integer(S_ADDRESSRAM));
-    VRAMIN(to_integer(S_ADDRESSRAM)) <= std_logic_vector(DATA) when S_WRAM = '1' and rising_edge(clk);
+    /*RAMIN <= "ZZZZZZZZ" when S_WRAM = '0' else std_logic_vector(DATA);
+    RW <= S_WRAM and (not CLKS);
+    */
+    REG <=signed('0'&RAMIN) when S_WRAM = '1' else "000000000";
+    
+	 RAMIN <= VRAMIN(to_integer(S_ADDRESSRAM));
+    VRAMIN(to_integer(S_ADDRESSRAM)) <= std_logic_vector(DATA) when S_WRAM = '1';
 
-
-    P1OUT <= P1;--; when CLKS'event and CLKS = '1' ;
-    P1 <= std_logic_vector(DATA) when RESET = '1' and EN_PORT = '1' else "ZZZZZZZZ"; --CSC(2) = EN_PORT
+    P1OUT <= S_P1OUT;
+    S_P1OUT <= --x"00" when RESET = '0' and clks'event and clks = '1' else
+                P1 when CLKS'event and CLKS = '1';
+    P1 <= std_logic_vector(DATA) when  EN_PORT = '1' else 
+            S_P1OUT;-- else "ZZZZZZZZ"; --CSC(2) = EN_PORT
     ------------------------------ 1SEQUENCER -----------------------------
     -- Program Counter -- 
 	PCIN <= std_logic_vector(S_PCIN);
-    S_PCIN  <= (PC_A) when clk'event and clk = '0';
+    S_PCIN  <= (PC_A) when clks'event and clks = '1';
 
     PC_A <=
-        x"0F" when PC_A >= x"0F" else
+        x"00" when PC_A >= x"21" and RESET = '1' else
         (others => '0') when RESET = '0' else
         DATA + 2 when PCL = '1' else
-        PC_A + 1 when rising_edge(clk) and PCL = '0' else
+        PC_A + 1 when rising_edge(clks) and PCL = '0' else
         PC_A;
 
     ------------------------------ 2Registers ------------------------------
@@ -159,19 +186,18 @@ begin
     -- Flag Register --
     FLAGOUT <= FLAGIN when CLKS'event and CLKS = '1' ;
     FLAGIN <= "0000" when RESET = '1' else 
-              ZERO & CARRY & NEG & OVF;
+              F_ZERO & F_CARRY & F_NEG & F_OVF;
     ---------------------------- 3Arithmetic Logic Unit ----------------------------
   
     -- ALU --
-    AC <= signed('0'&std_logic_vector(DATA)) when CLKS'event and CLKS = '1'  and RESET = '1' and EN_AC = '1' else
-          "000000000" when CLKS'event and CLKS = '1';
+    AC <= "000000000" when RESET = '0' else 
+        signed('0'&std_logic_vector(DATA)) when CLKS'event and CLKS = '1'  and EN_AC = '1';
         
-    REG <=signed('0'&RAMIN);
  
-    ZERO <='0' when ROMIN = x"4C" else '1' when INA = "000000000" else '1' when ROMIN = x"4B" else  '0';
-    CARRY<='1' when ROMIN = x"47" else '0' when ROMIN = x"48" else INA(8);
-    NEG  <='1' when ROMIN = x"49" else '0' when ROMIN = x"4C" else INA(7);
-    OVF  <='1' when ROMIN = x"4D" else '0' when ROMIN = x"4E" else CARRY xor NEG;
+    F_ZERO <='0' when ROMIN = x"4C" else '1' when INA = "000000000" else '1' when ROMIN = x"4B" else  '0';
+    F_CARRY<='1' when ROMIN = x"47" else '0' when ROMIN = x"48" else INA(8);
+    F_NEG  <='1' when ROMIN = x"49" else '0' when ROMIN = x"4C" else INA(7);
+    F_OVF  <='1' when ROMIN = x"4D" else '0' when ROMIN = x"4E" else F_CARRY xor F_NEG;
     with ROMIN select
     INA <= 
         AC + REG + FLAGOUT(2)         when "00000000" | "00000001" | "00000010" | "00000011" |
@@ -212,7 +238,7 @@ begin
 
     -- Decoder --
     PCL <= '1'  when IBI="01" or (ROMIN=x"5D" and IBI="00" and FLAGIN(3)='1') or (ROMIN=x"5E" and IBI="00" and FLAGIN(2)='1') or (ROMIN=x"5F" and IBI="00" and FLAGIN(1)='1') or (ROMIN=x"60" and IBI="00" and FLAGIN(0)='1') or (ROMIN=x"5A")
-                or (IBI="10" and ROMIN=x"5B") or (IBI="00" and ROMIN=x"5C") else '0';
+                or (IBI="01" and ROMIN=x"5B") or (IBI="11") else '0';
 
     EN_AC <='1' when ((IBI="00" and (ROMIN<x"10" or (ROMIN>x"17" and ROMIN<x"38") or (ROMIN>x"3F" and ROMIN<x"47") or ROMIN=x"50" or ROMIN=x"51" or ROMIN=x"53")) or IBI="10")  
             else '0';
@@ -220,9 +246,8 @@ begin
     EN_PORT <= '1' when ROMIN = x"50"--(IBI="00" and ROMIN=x"40") or (IBI="00" and ROMIN=x"61") or (IBI="10" and ROMIN=x"5B") or ()
             else '0';
 
-    SPUD <= "01" when (IBI="10" and ROMIN=x"5B") else
-            "10" when (IBI="00" and ROMIN=x"56") or (IBI="00" and ROMIN=x"5C") else
-            "11" when (IBI="00" and ROMIN=x"61") else 
+    SPUD <= "01" when (IBI="00" and ROMIN=x"5B") else
+            "10" when (IBI="00" and ROMIN=x"56") or (ROMIN=x"5C") else
             "00";
 
     XUD <=  "01" when IBI="00" and ROMIN=x"58" else
@@ -231,27 +256,28 @@ begin
             "00";
 
     S_WRAM <= '1' when (IBI="00" and ((ROMIN>x"37" and ROMIN<x"40") or ROMIN=x"54"  or ROMIN=x"5B")) 
-                or   (IBI="00" and (ROMIN=x"56")) else '0';
+                or   (IBI="00" and (ROMIN=x"56" or ROMIN = x"61"))  else '0';
 
     SELR <= '0'&'0'&'0'&'0'&'0' & ROMIN(2 downto 0) when (ROMIN(7 downto 3) < "01000") else "00000000";
     SELL <= 
-        "11" when IBI="00" and ((ROMIN>x"54" and ROMIN<x"57") or (ROMIN>x"5A" and ROMIN<x"5D") or ROMIN=x"5B") else
+        "11" when (IBI="00" and ((ROMIN>x"54" and ROMIN<x"57") or (ROMIN>x"5A" and ROMIN<x"5D") or ROMIN=x"5B"))or IBI = "11" else
         "10" when IBI="00" and (ROMIN>x"52" and ROMIN<x"55") else
         "00";            
 
     SELD <= 
-        M_INA when IBI="00" and ((ROMIN>x"37" and ROMIN<x"40") or ROMIN=x"40" or ROMIN=x"42" or ROMIN=x"44" or ROMIN=x"46"  or ROMIN = x"50") else
+        M_INA when (IBI="00" and ((ROMIN>x"37" and ROMIN<x"40") or ROMIN=x"40" or ROMIN=x"42" or ROMIN=x"44" or ROMIN=x"46"  or ROMIN = x"50" or  ROMIN = x"61"))  else
         M_P0IN when IBI="00" and ROMIN=x"4F" else
-        M_SPIN when IBI="00" and ROMIN=x"5C" else
+        --M_SPIN when IBI="00" and ROMIN=x"5C" else
         M_XIN when IBI="00" and ROMIN=x"51" else
-        M_RAMIN when IBI="00" and ((ROMIN>x"2F" and ROMIN<x"38") or ROMIN=x"53" or ROMIN=x"55" or ROMIN=x"5C") else
+        M_RAMIN when (IBI="00" and ((ROMIN>x"2F" and ROMIN<x"38") or ROMIN = x"5C" or ROMIN=x"53" or ROMIN=x"55" or ROMIN=x"5C")) or IBI = "11" else
         M_PCIN when IBI="00" and ROMIN=x"5B" else
         M_ROMIN when IBI="01" or IBI="10" else
         M_ALU;
 
     IBO <= 
         "01" when IBI="00" and ((ROMIN=x"5D" and FLAGIN(3)='1') or (ROMIN=x"5E" and FLAGIN(2)='1') or (ROMIN=x"5F" and FLAGIN(1)='1') or (ROMIN=x"60" and FLAGIN(0)='1') or ROMIN=x"5A" or ROMIN=x"5B" or ROMIN=x"5B") else
-        "10" when (ROMIN=x"57") else
+        "10" when ROMIN = x"57" else
+        "11" when ROMIN = x"5C" else
         "00";
 
     -- Decoder Machine States --
@@ -260,71 +286,3 @@ begin
         "00" when CLKS'event and CLKS ='1'; 
 
 end juve3dstudio;
-
-
-/*
-
-    -- WRAM Escribir Ram
-    
-    -- XD  XU  Puntero X
-    -- SPD SPU Stack Pointer
-    --  0   0  sin cambio
-    --  0   1  decremento
-    --  1   0  incremento (pre)
-    --  1   1  carga
-
-    -- EN_PORT Mandar puerto salida ENP
-    -- EN_AC Escribir o modifir el Acumulador ENA
-    -- PCL Modificar Program Counter
-
-    
-    -- SCL Selector Ram MRAM
-    -- SCR Selector Registro M
-    -- SCD Selector de dato
-    -- IBI IBO Maquina de estados del decodificador 
-
-
-ADC A,Rs |0x"00" - 0x"07"
-SBC A,Rs |0x"08" - 0x"0F"
-CPC A,Rs |0x"10" - 0x"17"
-AND A,Rs |0x"18" - 0x"1F"
-ORL A,Rs |0x"20" - 0x"27"
-EOR A,Rs |0x"28" - 0x"2F"
-MOV A,Rs |0x"30" - 0x"37"
-MOV Rs,A |0x"38" - 0x"3F"
-COM A    |0x"40"
-NEG A    |0x"41"
-INC A    |0x"42"
-DEC A    |0x"43"
-CLR A    |0x"44"
-ROL A    |0x"45"
-ROR A    |0x"46"
-SET C    |0x"47"
-CLR C    |0x"48"
-SET N    |0x"49"
-CLR N    |0x"4A"
-SET Z    |0x"4B"
-CLR Z    |0x"4C"
-SET V    |0x"4D"
-CLR V    |0x"4E"
-MOV A,PO |0x"4F"
-MOV P1,A |0x"50"
-MOV A,X  |0x"51"
-MOV X,A  |0x"52"
-MOV A,M  |0x"53"
-MOV M,A  |0x"54"
-POP A    |0x"55"
-PUSH A   |0x"56"
-MOV A,K  |0x"57"
-INC X    |0x"58"
-DEC X    |0x"59"
-JMP      |0x"5A"
-CALL     |0x"5B"
-RET      |0x"5C"
-BREQ     |0x"5D"
-BRCS     |0x"5E"
-BRMI     |0x"5F"
-BRVS     |0x"60"
-MOV SP,A |0x"61"
-NOP      |0x"FF"
-*/
